0ba4d2802a2d40e91d4114fb86f77005dc00fa4e
SylvainJunca <sylvain.junca@gmail.com>
2023-11-09 09:41:55 -0500

    adds execute script that runs askQuestion prompt

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-export { modThree } from "./modThree";
+import { modThree } from "./modThree";
+import * as readline from "readline";
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuestion() {
+    rl.question("Enter a binary number (or 'quit' to exit): ", (answer) => {
+      if (answer.toLowerCase() === 'quit') {
+        console.log("Goodbye, have an amazing day! :)")
+        rl.close();
+      } else {
+        try {
+          const response = modThree(answer);
+          console.log(`The modulo 3 of ${answer} is ${response}`);
+        } catch (e) {
+          if (e instanceof Error) {
+            console.error(e.message);
+          } else {
+            console.error(e);
+          }
+        }
+        askQuestion();
+      }
+    });
+  }
+
+  askQuestion();
