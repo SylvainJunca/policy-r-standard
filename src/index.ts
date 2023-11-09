74b9436334d10d0d72d3698110d0619130d16ef2
SylvainJunca <sylvain.junca@gmail.com>
2023-11-08 19:25:09 -0500

    working function to solve the standard exercise

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Setup")
+enum State {
+  S0 = 0,
+  S1 = 1,
+  S2 = 2,
+}
+
+function transition(state: State, input: string): State {
+  const transTable: { [key in State]: { [key: string]: State } } = {
+    [State.S0]: { "0": State.S0, "1": State.S1 },
+    [State.S1]: { "0": State.S2, "1": State.S0 },
+    [State.S2]: { "0": State.S1, "1": State.S2 },
+  };
+  return transTable[state][input];
+}
+
+function modThree(input: string): number {
+  let currentState: State = State.S0;
+  for (let i = 0; i < input.length; i++) {
+    currentState = transition(currentState, input[i]);
+  }
+  return currentState;
+}
+
+console.log(modThree("1101"));
+console.log(modThree("1110"));
+console.log(modThree("1111"));
