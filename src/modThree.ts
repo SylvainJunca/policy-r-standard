1c8f4ce6b63887e8bf205f3513d4daade41f75e8
SylvainJunca <sylvain.junca@gmail.com>
2023-11-09 11:01:14 -0500

    adds eslint + airbnb rules + prettier

--- src/modThree.ts
+++ src/modThree.ts
@@ -1,34 +1,34 @@
-export enum State {
+export enum STATE {
   S0 = 0,
   S1 = 1,
   S2 = 2,
 }
-const alphabet: string[] = ["0", "1"];
+const ALPHABET: string[] = ['0', '1'];
 
 export function validateInput(input: string, alphabet: string[]): void {
-  const regex = new RegExp(`^[${alphabet.join("")}]+$`);
+  const regex = new RegExp(`^[${alphabet.join('')}]+$`);
   if (!regex.test(input)) {
-    throw new Error(`Input must only contain ${alphabet.join(", ")}`);
+    throw new Error(`Input must only contain ${alphabet.join(', ')}`);
   }
 }
 
-export function transition(state: State, input: string): State {
-  const transTable: { [key in State]: { [key: string]: State } } = {
-    [State.S0]: { "0": State.S0, "1": State.S1 },
-    [State.S1]: { "0": State.S2, "1": State.S0 },
-    [State.S2]: { "0": State.S1, "1": State.S2 },
+export function transition(state: STATE, input: string): STATE {
+  const transTable: { [key in STATE]: { [key: string]: STATE } } = {
+    [STATE.S0]: { 0: STATE.S0, 1: STATE.S1 },
+    [STATE.S1]: { 0: STATE.S2, 1: STATE.S0 },
+    [STATE.S2]: { 0: STATE.S1, 1: STATE.S2 },
   };
   if (!(state in transTable) || !(input in transTable[state])) {
-    throw new Error(`Invalid state or input: state=${state}, input=${input}`);
+    throw new Error(`Invalid STATE or input: STATE=${STATE}, input=${input}`);
   }
   return transTable[state][input];
 }
 
 export function modThree(input: string): number {
-  validateInput(input, alphabet);
-  let currentState: State = State.S0;
-  for (let i = 0; i < input.length; i++) {
-    currentState = transition(currentState, input[i]);
-  }
+  validateInput(input, ALPHABET);
+  let currentState: STATE = STATE.S0;
+  input.split('').forEach((char) => {
+    currentState = transition(currentState, char);
+  });
   return currentState;
 }
--- tests/modThree.test.ts
+++ tests/modThree.test.ts
@@ -1,60 +1,65 @@
-import { validateInput, transition, modThree, State } from "../src/modThree";
+import {
+  validateInput,
+  transition,
+  modThree,
+  STATE,
+} from '../src/modThree';
 
-describe("validateInput", () => {
-  it("should not throw an error for valid input", () => {
-    expect(() => validateInput("01", ["0", "1"])).not.toThrow();
-    expect(() => validateInput("00", ["0", "1"])).not.toThrow();
-    expect(() => validateInput("11", ["0", "1"])).not.toThrow();
+describe('validateInput', () => {
+  it('should not throw an error for valid input', () => {
+    expect(() => validateInput('01', ['0', '1'])).not.toThrow();
+    expect(() => validateInput('00', ['0', '1'])).not.toThrow();
+    expect(() => validateInput('11', ['0', '1'])).not.toThrow();
   });
 
-  it("should throw an error for invalid input", () => {
-    expect(() => validateInput("02", ["0", "1"])).toThrow();
-    expect(() => validateInput("ab", ["0", "1"])).toThrow();
-    expect(() => validateInput("", ["0", "1"])).toThrow();
-  });
-});
-
-describe("transition", () => {
-  it("should transition correctly from state S0", () => {
-    expect(transition(State.S0, "0")).toBe(State.S0);
-    expect(transition(State.S0, "1")).toBe(State.S1);
-  });
-
-  it("should transition correctly from state S1", () => {
-    expect(transition(State.S1, "0")).toBe(State.S2);
-    expect(transition(State.S1, "1")).toBe(State.S0);
-  });
-
-  it("should transition correctly from state S2", () => {
-    expect(transition(State.S2, "0")).toBe(State.S1);
-    expect(transition(State.S2, "1")).toBe(State.S2);
-  });
-
-  it("should handle invalid input", () => {
-    expect(() => transition(State.S0, "2")).toThrow();
-    expect(() => transition(State.S1, "a")).toThrow();
-    expect(() => transition(State.S2, "")).toThrow();
-  });
-
-  it("should handle invalid state", () => {
-    expect(() => transition(3 as State, "0")).toThrow();
+  it('should throw an error for invalid input', () => {
+    expect(() => validateInput('02', ['0', '1'])).toThrow();
+    expect(() => validateInput('ab', ['0', '1'])).toThrow();
+    expect(() => validateInput('', ['0', '1'])).toThrow();
   });
 });
 
-describe("modThree", () => {
+describe('transition', () => {
+  it('should transition correctly from STATE S0', () => {
+    expect(transition(STATE.S0, '0')).toBe(STATE.S0);
+    expect(transition(STATE.S0, '1')).toBe(STATE.S1);
+  });
+
+  it('should transition correctly from STATE S1', () => {
+    expect(transition(STATE.S1, '0')).toBe(STATE.S2);
+    expect(transition(STATE.S1, '1')).toBe(STATE.S0);
+  });
+
+  it('should transition correctly from STATE S2', () => {
+    expect(transition(STATE.S2, '0')).toBe(STATE.S1);
+    expect(transition(STATE.S2, '1')).toBe(STATE.S2);
+  });
+
+  it('should handle invalid input', () => {
+    expect(() => transition(STATE.S0, '2')).toThrow();
+    expect(() => transition(STATE.S1, 'a')).toThrow();
+    expect(() => transition(STATE.S2, '')).toThrow();
+  });
+
+  it('should handle invalid STATE', () => {
+    expect(() => transition(3 as STATE, '0')).toThrow();
+  });
+});
+
+describe('modThree', () => {
   it('should return 1 for input "1101"', () => {
-    expect(modThree("1101")).toBe(1);
+    expect(modThree('1101')).toBe(1);
   });
 
   it('should return 0 for input "1110"', () => {
-    expect(modThree("1110")).toBe(2);
+    expect(modThree('1110')).toBe(2);
   });
 
   it('should return 1 for input "1111"', () => {
-    expect(modThree("1111")).toBe(0);
+    expect(modThree('1111')).toBe(0);
   });
 
-  it("should throw an error for invalid input", () => {
-    expect(() => modThree("1123")).toThrow();
+  it('should throw an error for invalid input', () => {
+    expect(() => modThree('1123')).toThrow();
   });
 });
